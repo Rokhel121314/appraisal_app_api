43772cd7c8cd4e0569f192690185e91504eb5de1
Rokhel121314 <jerickddelacruz@gmail.com>
2024-07-20 20:55:32 +0800

    updated origin

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ app.use(
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: ["Content-Type", "Authorization"],
-    origin: ["https://appraisal-app-ui.vercel.app", "http://localhost:3000"], // whatever ports you used in frontend
+    origin: ["appraisal-app.vercel.app", "http://localhost:3000"], // whatever ports you used in frontend
   })
 );
 app.use(cookieParser());
